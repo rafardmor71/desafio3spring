17b239c70a3409645192ddac42a332970462dcd9
Rafa Dominguez <rafa.realmadrid.rdm@gmail.com>
2021-11-10 14:20:32 +0100

    Primer commit

---
+++ src/main/java/com/example/desafiospring3/service/ClienteServiceI.java
@@ -0,0 +1,16 @@
+package com.example.desafiospring3.service;
+
+import java.util.List;
+
+import com.example.desafiospring3.entities.Cliente;
+
+public interface ClienteServiceI {
+	
+	void añadirCliente(Cliente cliente);
+	void modificarCliente(Cliente cliente);
+	List<Cliente> listarTodosLosClientes();
+	void borrarCliente(Cliente cliente);
+	List<Cliente> buscarClientePorNombreYApellidos(String nombre, String apellidos);
+	Cliente buscarClientePorId(Long idCliente);
+	
+}
